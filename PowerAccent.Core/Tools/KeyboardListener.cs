bb01fae2124fb6b85ffa0cfc6f86e95e9b8f7c1d
Damien LEROY <dleroy@veepee.com>
2022-09-02 17:17:26 +0200

    remove syskeys causing trouble

--- PowerAccent.Core/Tools/KeyboardListener.cs
+++ PowerAccent.Core/Tools/KeyboardListener.cs
@@ -75,14 +75,11 @@ internal class KeyboardListener : IDisposable
     {
         if (nCode >= 0)
             if (wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_KEYDOWN ||
-                wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_KEYUP ||
-                wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_SYSKEYDOWN ||
-                wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_SYSKEYUP)
+                wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_KEYUP)
             {
                 //Captures the character(s) pressed only on WM_KEYDOWN
                 var chars = InterceptKeys.VKCodeToString((uint)Marshal.ReadInt32(lParam),
-                    wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_KEYDOWN ||
-                     wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_SYSKEYDOWN);
+                    wParam.ToUInt32() == (int)InterceptKeys.KeyEvent.WM_KEYDOWN);
 
                 if (!hookedKeyboardCallbackAsync.Invoke((InterceptKeys.KeyEvent)wParam.ToUInt32(), Marshal.ReadInt32(lParam), chars))
                 {
@@ -108,19 +105,11 @@ internal class KeyboardListener : IDisposable
                 if (KeyDown != null)
                     return KeyDown.Invoke(this, new RawKeyEventArgs(vkCode, false, character));
                 break;
-            case InterceptKeys.KeyEvent.WM_SYSKEYDOWN:
-                if (KeyDown != null)
-                    return KeyDown.Invoke(this, new RawKeyEventArgs(vkCode, true, character));
-                break;
 
             // KeyUp events
             case InterceptKeys.KeyEvent.WM_KEYUP:
                 if (KeyUp != null)
                     return KeyUp.Invoke(this, new RawKeyEventArgs(vkCode, false, character));
-                break;
-            case InterceptKeys.KeyEvent.WM_SYSKEYUP:
-                if (KeyUp != null)
-                    return KeyUp.Invoke(this, new RawKeyEventArgs(vkCode, true, character));
                 break;
 
             default:
